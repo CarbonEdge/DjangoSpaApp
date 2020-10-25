83e6bb08d2add83ddd5ff7ebd8d3663ae31bc791
CarbonEdge <wholetree65@gmail.com>
2020-10-25 15:56:58 +0200

    test working app

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry : "./leadmananager/frontend/src/index.js",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  }
